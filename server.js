c122b9e6b5bfdeffa28888bba19cbc6a9717a8d0
Mohseen365 <mohammedmohseen365@gmail.com>
2022-12-11 17:40:57 +0530

    response directly from server

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
-const server = http.createServer((req, res) => {
-  console.log('');
+const server = http.createServer((req, res) => { //it creates the server but response tab hi jayega jab http://localhost:3000/ se request aayegi
+  console.log("request from browser to server");
+  res.setHeader('Content-type','text/html');
+  res.write("<h1>hello world</h1>");
+  res.write("<h2>Bang bang</h>");
+  res.end("res end");
+
+})
+server.listen(3000,'localhost', ()=>{ //browser se request lega
+  console.log("server is listening on port 3000");
+
 })
 
 
-//console.log();
