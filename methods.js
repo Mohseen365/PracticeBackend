74ff3c0da1561b59b061e675e59c517990924d3e
Mohseen365 <mohammedmohseen365@gmail.com>
2022-12-20 23:32:37 +0530

    params

--- methods.js
+++ methods.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());// isse use krna padta he post req ko chalane ke liye
+user = {};
 users = [
   {
     id: 1,
@@ -22,6 +23,32 @@ users = [
 app.get('/user', (req, res) => {
   res.send(user);
 })
+
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  const filteredData = users.filter(userObj => {
+    return (userObj.id == id)
+  })
+  res.json(filteredData);
+})
+
+// This will not work we have to make query
+// app.get('/users/:name', (req, res) => {
+//   const name = req.params.name;
+//   const filteredData = users.filter(userObj => {
+//     return (userObj.name == name)
+//   })
+//   res.json(filteredData);
+// })
+
+app.get('/users', (req, res) => {
+  let {name, age} = req.query;
+  let filteredData = users.filter(userObj => {
+    return (userObj.name == name && userObj.age == age)
+  })
+  res.json(filteredData);
+})
+
 
 app.post('/user',(req, res) => {
   console.log(req.body);
