9ce52695ad8ce731ed1dfbcaf6123652e25968eb
Mohseen365 <mohammedmohseen365@gmail.com>
2022-12-29 17:45:49 +0530

    Create User through code

--- foodApp/app.js
+++ foodApp/app.js
@@ -180,3 +180,40 @@ mongoose.connect(db_link)
     .catch(function (err) {
         console.log(err);
     });
+
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+    minLength: 7,
+  }
+});
+
+const userModel = mongoose.model("userModel", userSchema);
+
+(async function createUser() {
+  let user = {
+    name: "Suresh",
+    email: "Suresh@gmail.com",
+    password: "12345678",
+    confirmPassword: "12345678"
+  };
+  let data = await userModel.create(user);
+  console.log(data);
+
+})();
